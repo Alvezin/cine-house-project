0d97daa5a44b6456d79db3c43240c7d65bcf9a06
Gabriel Alves <99101659+Alvezin@users.noreply.github.com>
2022-06-21 20:42:09 -0300

    Criacao da pagina de sinopse

--- src/components/movieDetailsComponents/MainDataFromMovie.vue
+++ src/components/movieDetailsComponents/MainDataFromMovie.vue
@@ -1,20 +1,31 @@
 <template>
-  <section :style="{backgroundImage: `url(https://image.tmdb.org/t/p/original/${state.image})`}">
-    <h2>{{ state.movie.title }}</h2>
-  </section>
+  <div>
+    <section
+        :style="{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${state.image})`,
+        }"
+    >
+        <h1 class="titulo">{{ state.movie.title }}</h1>
+    </section>
+    <movie-description :movie="state.movie"/>
+  </div>
 </template>
 
 <script>
+import MovieDescription from "./MovieDescription.vue"
 import { reactive } from "@vue/reactivity";
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
 import { computed, onMounted } from "@vue/runtime-core";
 export default {
   name: "MainDataFromMovie",
+  components: {
+    MovieDescription
+  },
   setup() {
     const state = reactive({
       movie: {},
-      image: ""
+      image: "",
     });
     const route = useRoute();
     const store = useStore();
@@ -22,7 +33,7 @@ export default {
       store.dispatch("getMovieData", route.params.movieId);
     });
     state.movie = computed(() => store.state.movieDetails.movie);
-    state.image = computed(() => state.movie.backdrop_path)
+    state.image = computed(() => state.movie.backdrop_path);
     return {
       state,
     };
@@ -32,8 +43,15 @@ export default {
 
 <style lang="scss" scoped>
 section {
-  min-height: 50vh;
+  min-height: 60vh;
   background-repeat: no-repeat;
   background-size: cover;
+  background-attachment: fixed;
+  box-shadow: inset 0px 0px 10px black;
+  & .titulo {
+    color: white;
+    text-transform: uppercase;
+    font-size: 31px;
+  }
 }
 </style>
---
+++ src/components/movieDetailsComponents/MovieDescription.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="flex-box" >
+        <div >
+            <img :src="`https://image.tmdb.org/t/p/w200/${movie.poster_path}`" alt="">
+        </div>
+        <div class="sinopse">
+            <h4>{{ movie.title }}</h4>
+            <p>{{ movie.overview }}</p>
+            <ul class="genre-list">
+                <li
+                    v-for="genre in movie.genres" :key="genre.id"
+                >{{ genre.name }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "MovieDescription",
+  props: ["movie"]
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/var.scss";
+    .flex-box {
+        background-color: $black-var;
+        color: white;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        height: 100vh;
+        & .sinopse {
+            width: 60%;
+            text-align: justify;
+            line-height: 40px;
+            text-indent: 15px;
+            & h4 {
+                margin: 0 0 0 10px;
+            }
+            & .genre-list {
+                display: flex;
+                flex-direction: row;
+                list-style: none;
+                color: $red-var;
+                font-weight: bold;
+            }
+        }
+        & img {
+            border-radius: 8px;
+        }
+    }
+
+</style>
